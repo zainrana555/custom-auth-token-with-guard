83d9fc2c096461a9b7d378c8984427caf0f6a715
zainrana555 <rana.zain555@gmail.com>
2022-01-31 11:36:42 +0500

    first commit

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Firebase\JWT\JWT;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/auth/register", name="register", methods={"POST"})
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        if (!$request->get('email')) {
+            return $this->json([
+                'Error' => 'Email is required'
+            ]);
+        }
+        if (!$request->get('password')) {
+            return $this->json([
+                'Error' => 'Password is required'
+            ]);
+        }
+
+        $password = $request->get('password');
+        $email = $request->get('email');
+        $user = new User();
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setEmail($email);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        return $this->json([
+            'user' => $user->getEmail()
+        ]);
+    }
+
+    /**
+     * @Route("/auth/login", name="login", methods={"POST"})
+     */
+    public function login(Request $request, UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
+    {
+            $user = $userRepository->findOneBy([
+                    'email'=>$request->get('email'),
+            ]);
+            if (!$user || !$encoder->isPasswordValid($user, $request->get('password'))) {
+                    return $this->json([
+                        'message' => 'email or password is wrong.',
+                    ]);
+            }
+           $payload = [
+               "user" => $user->getUsername(),
+               "exp"  => (new \DateTime())->modify("+5 minutes")->getTimestamp(),
+           ];
+
+
+            $jwt = JWT::encode($payload, $this->getParameter('jwt_secret'), 'HS256');
+            return $this->json([
+                'message' => 'success!',
+                'token' => sprintf('Bearer %s', $jwt),
+            ]);
+    }
+}
